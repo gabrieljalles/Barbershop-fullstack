6c4a891495b4af40018fb55843de2b889181bd05
Gabriel Jalles <gabriel.jalleswork@hotmail.com>
2024-09-08 15:01:15 -0300

    complete: barberShopItem component

--- components/BarbershopItem.tsx
+++ components/BarbershopItem.tsx
@@ -2,6 +2,8 @@ import { Barbershop } from "@prisma/client"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
+import { StarIcon } from "lucide-react"
 
 interface BarbershopItemProps {
   barbershop: Barbershop
@@ -19,6 +21,13 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
             src={barbershop.imageUrl}
             alt={barbershop.name}
           />
+          <Badge
+            className="absolute left-2 top-2 space-x-1"
+            variant="secondary"
+          >
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold"> 5,0</p>
+          </Badge>
         </div>
         {/*TEXTO*/}
         <div className="px-1 py-3">
