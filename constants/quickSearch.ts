b79fc4b6e134387b2aa6977685afd3796cb1c6aa
Gabriel Jalles <gabriel.jalleswork@hotmail.com>
2024-10-23 19:26:37 -0300

    fix: updated deploy icons

--- constants/quickSearch.ts
+++ constants/quickSearch.ts
@@ -6,32 +6,32 @@ interface QuickSearchOptionProps {
 
 export const quickSearchOptions: QuickSearchOptionProps[] = [
   {
-    imageUrl: "@/app/_public/cabelo.svg",
+    imageUrl: "/cabelo.svg",
     title: "Cabelo",
     alt: "Pesquisa rápida de cabelo",
   },
   {
-    imageUrl: "@/app/_public/barba.svg",
+    imageUrl: "/barba.svg",
     title: "Barba",
     alt: "Pesquisa rápida de barba",
   },
   {
-    imageUrl: "@/app/_public/acabamento.svg",
+    imageUrl: "/acabamento.svg",
     title: "Acabemento",
     alt: "Pesquisa rápida de acabemento",
   },
   {
-    imageUrl: "@/app/_public/sobrancelha.svg",
+    imageUrl: "/sobrancelha.svg",
     title: "Sobrancelha",
     alt: "Pesquisa rápida de sobrancelha",
   },
   {
-    imageUrl: "@/app/_public/massagem.svg",
+    imageUrl: "/massagem.svg",
     title: "Massagem",
     alt: "Pesquisa rápida de massagem",
   },
   {
-    imageUrl: "@/app/_public/hidratacao.svg",
+    imageUrl: "/hidratacao.svg",
     title: "Hidratação",
     alt: "Pesquisa rápida do hidratacao",
   },
